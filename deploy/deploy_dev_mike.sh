9fd7f1b1325da9e0f32ebebb77e865d6d1720262
Mike Hung <mike@powerarena.com>
2023-08-13 00:32:54 +0800

    fix wrong command in dev_mike.sh

--- deploy/deploy_dev_mike.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker compose -f docker-compose.yml -f docker-compose.dev.yml build
-CVAT_HOST=192.168.0.123 docker compose -f docker-compose.yml -f docker-compose.dev.yml up
-
