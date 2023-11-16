91445f311460acd8fabf8d0ffcf595588d8ced32
ChrisZhu <tw49@powerarena.com>
2023-11-16 17:16:08 +0800

    update deploy script

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -1,5 +1,12 @@
 ENV_NAME=$1
+export CVAT_SERVER_VERSION=$2
+
+echo "ENV_NAME: $ENV_NAME"
+echo "CVAT_SERVER_VERSION: $2"
+echo "CVAT_VERSION: $3"
+
 export CVAT_HOST=cvat-$ENV_NAME.standalone.powerarena.com
 
-docker-compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml down
-docker-compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml up -d --build
+
+docker compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml down
+docker compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml up -d --build
